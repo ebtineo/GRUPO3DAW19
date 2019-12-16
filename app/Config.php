52a0da9fb6647f61eab6b903c4691865d8c4fb3c
segisito <55485789+segisito@users.noreply.github.com>
2019-12-16 12:14:32 +0100

    Primer commit Sergiy

---
+++ app/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+ class Config
+ {
+     static public $mvc_bd_hostname = "localhost:3306";
+     static public $mvc_bd_usuario  = "php";
+     static public $mvc_bd_clave    = "phpz3rh10";
+     static public $mvc_bd_nombre   = "alimentos32019";
+     static public $mvc_vis_css     = "estilo.css";
+    
+ }
+?>
